4bbf83b1a042fc407df93a0fb5d83dde26e1bf3f
Benyamin Jamialahmadi <b2jamial@amgradmac262.nexus.uwaterloo.ca>
2022-08-24 18:02:14 -0400

    initialization script

---
+++ initialization.sh
@@ -0,0 +1,12 @@
+cd ..
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --disable-pip-version-check --no-cache-dir ./
+cd ..
+mkdir data
+cd data
+cp /content/drive/MyDrive/Antibody/Data/Antigens/antigens_split_processed_windowed_250_125.tar.gz .
+tar -xzvf antigens_split_processed_windowed_250_125.tar.gz
+cp /content/drive/MyDrive/Antibody/Data/OAS/Cleaned/*.pth .
+cp /content/drive/MyDrive/Antibody/Data/Parallel/*.pth .
+cd ../AntiXLM
