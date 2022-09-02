2cf9b0ec43283a02cdbaabdfec3e63a993c36432
ben <benyamin.jami76@gmail.com>
2022-09-01 21:48:22 -0400

    fix

--- xlm/evaluation/evaluator.py
+++ xlm/evaluation/evaluator.py
@@ -581,7 +581,7 @@ def convert_to_text(batch, lengths, dico, params, mode='mt'):
         step = len(lengths)
         i = 0
 
-        while step != 0:
+        while step > 0:
             for j in range(len(lengths)):
                 if i < lengths[j]-1:
                     sentences[j].append(dico[batch[j + i*step]])
